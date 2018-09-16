69f31e5d5561a1c8a9f97ed038a06587d994639a
Nayan <nayanrathece@gmail.com>
2018-09-16 15:21:11 +0530

    Adding UI Automator changes for Instrumentation test.

--- app/src/androidTest/java/com/mob/redmart/MainActivityTest.java
+++ app/src/androidTest/java/com/mob/redmart/MainActivityTest.java
@@ -4,40 +4,62 @@ package com.mob.redmart;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
-
-import com.mob.redmart.view.MainActivity;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiScrollable;
+import android.support.test.uiautomator.UiSelector;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule
-            = new ActivityTestRule<MainActivity>(MainActivity.class);
-
     private UiDevice mDevice;
 
-
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-//        assertThat(mDevice, notNullValue());
+        assertThat(mDevice, notNullValue());
+
         // Start from the home screen
         mDevice.pressHome();
 
     }
 
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void launchApp() throws UiObjectNotFoundException, InterruptedException {
 
+        UiObject mainWindow = mDevice.findObject(new UiSelector().packageName("com.mob.redmart").description("RedMartTask"));
+
+        mainWindow.waitForExists(2000);
+
+        mainWindow.clickAndWaitForNewWindow();
+        assertThat(mainWindow, notNullValue());
+
+        UiScrollable mRecyclerView = new UiScrollable(new UiSelector().className("android.support.v7.widget.RecyclerView"));
+
+        for (int i = 0; i < mRecyclerView.getChildCount(); i++) {
+            UiObject child = mRecyclerView.getChild(new UiSelector().className("android.widget.LinearLayout").index(i));
+
+            child.waitForExists(1000);
+            Thread.sleep(5000);
+            mDevice.pressBack();
+        }
+
+
+        mRecyclerView.scrollToEnd(50);
+        Thread.sleep(200);
     }
 
-    private class ActivityTestRule<T> {
+    @After
+    public void tearDown() {
+        mDevice = null;
     }
 }
